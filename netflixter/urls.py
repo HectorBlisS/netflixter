8d93835391c5be99fc8ee0354611966f2262b102
SaulSandovalM <saul_watch_dogs@outlook.com>
2016-08-09 10:10:15 -0500

    Errores

--- netflixter/urls.py
+++ netflixter/urls.py
@@ -13,9 +13,10 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib import admin
 from django.conf import settings
+from main import urls as UrlsMain
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
@@ -23,4 +24,5 @@ urlpatterns = [
         regex=r'^media/(?P<path>.*)$',
         view='django.views.static.serve',
         kwargs={'document_root':settings.MEDIA_ROOT}),
+    url(r'^raiz/$', include(UrlsMain,namespace="home")),      
 ]
