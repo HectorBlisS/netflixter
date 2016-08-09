3e73cf9d29a883127fe06554d17681ad302311f5
Serchcode <pe280591@uaeh.edu.mx>
2016-08-09 11:49:26 -0500

    urls detalle

--- netflixter/urls.py
+++ netflixter/urls.py
@@ -16,10 +16,12 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 from django.conf import settings
+from catalogo import urls as catalogoUrls
 from django.views.static import serve
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^netflixter/',include(catalogoUrls,namespace="catalogo"))
     url(
         regex=r'^media/(?P<path>.*)$',
         view=serve,
