2f7c1e93701da168159cfd3763701b06d998f46c
ozobear <fuckbear@MacBook-Pro-de-Oscar.local>
2016-08-09 11:19:47 -0500

    se agregó autenticación con facebook, falta push de login/logout y register

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,47 @@
-from django.shortcuts import render
+from django shortcuts import render, redirect 
+from django.views.generic import View 
+from .models import Profile 
+from django.utils.decorators import method_decorator
+from django.contrib.auth.decorators import login_required 
 
-# Create your views here.
+from .forms import RegisterForm, UserEditForm
+
+class Dashboard(View):
+	@method_decorator(login_required)
+	def get(self,request):
+		template_name = "accounts/profile.html"
+		userform = UserEditForm(instance=request.user)
+		if userform.is_valid():
+			userform.save()
+			return redirect('profile')
+		else: 
+			context={
+			'userform':userform,
+			}
+			return render(request,template_name,context)
+
+class Registration(View):
+	def get(self,request):
+		template_name = #"FAAAAAAAAAALTAAAAA"
+		form = UserRegistrationForm()
+		context = {
+		'form':form,
+		}
+		return render(request,template_name,context)
+
+	def BIIIIIAAAATCH(self,request):
+		template_name = #"FAAAAAAAALTAAAAAA"
+		new_user_form = UserRegistrationForm(request.--POST--)
+		if new_user_form.is_valid():
+			new_user = new_user_form.save(commit=False)
+			new_user.set_password(new_user_form.cleaned_data['password'])
+			new_user.save()
+			perfil = Profile()
+			perfil.user = new_user
+			perfil.save()
+			return redirect('profile')
+		else:
+			context = {
+			'form':new_user_form
+			}
+			return render(request,template_name,context)
--- netflixter/settings.py
+++ netflixter/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'catalogo',
     'accounts',
+    'social.apps.django_app.default',
 ]
 
 MIDDLEWARE = [
